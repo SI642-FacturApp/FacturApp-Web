dc4c1282a1c127584a8f135195df047cd8e068af
Pachieeee <sebaspacheco2003@gmail.com>
2024-11-11 00:06:29 -0500

    feat: added login components.

---
+++ src/IAM/components/login-form.component.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "login-form"
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <div class="pb-2">
+      <pv-input-text placeholder="Usuario..."/>
+    </div>
+    <div class="pb-2">
+      <pv-password
+          placeholder="Contraseña..."
+          :feedback="false"/>
+    </div>
+  </div>
+  <pv-button class="bg-black-alpha-90 hover:bg-black-alpha-50 border-round-xl border-none w-full">
+    Iniciar Sesión
+  </pv-button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/IAM/pages/login.component.vue
@@ -0,0 +1,32 @@
+<script>
+import LoginForm from "../components/login-form.component.vue";
+
+export default {
+  name: "login",
+  components: {LoginForm}
+}
+</script>
+
+<template>
+  <div>
+    <img src="../../assets/logo.png" alt="FacturApp logo" width="300px">
+    <login-form/>
+    <div>
+      ¿No tienes una cuenta?
+      <router-link to="/signup">Regístrate</router-link>
+    </div>
+    <div>
+      ¿Olvidaste tu contraseña?
+      <router-link to="/recovery">
+        Recupérala
+      </router-link>
+    </div>
+  </div>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
