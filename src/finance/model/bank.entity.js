7cfd0718b7b7fc94507f134a4a8c9534ee3f31af
P1ero0VL <u20211a620@upc.edu.pe>
2024-11-13 02:49:20 -0500

    feat(finance): added bank entity

---
+++ src/finance/model/bank.entity.js
@@ -0,0 +1,10 @@
+export class BankEntity {
+    constructor({id = '', name = '', tna = 0, tca = 0, image = ''}) {
+        this.id = id;
+        this.name = name;
+        this.tna = tna;
+        this.tea = tca;
+        this.image = image;
+    }
+
+}
