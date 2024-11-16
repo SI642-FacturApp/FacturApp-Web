765a3cb4680fed540f29de7bb02b87c5e09e3123
P1er0VL <u20211a620@upc.edu.pe>
2024-11-16 03:10:40 -0500

    feat(finance): updated banks component

--- src/finance/components/banks.component.vue
+++ src/finance/components/banks.component.vue
@@ -1,31 +1,58 @@
 <script>
-
-import {Button as PvButton, InputNumber as PvInputNumber} from "primevue";
+import {Bank} from "../model/bank.entity.js";
+import {FinanceService} from "../services/finance.service.js";
 
 export default {
   name: "banks",
-  components: {PvButton, PvInputNumber},
+  components: {},
 
   data() {
     return {
       banks: [],
+      bank: new Bank({}),
       dialogVisible: false,
-
-    }
+      financeService: new FinanceService()
+    };
   },
   methods: {
     clear() {
-      this.banks = [];
+      this.bank = new Bank({});
+    },
+    generateNewId() {
+      if (!this.banks || this.banks.length === 0) {
+        return '1';
+      }
+      const maxId = Math.max(...this.banks.map(bank => parseInt(bank.id, 10) || 0));
+      return (maxId + 1).toString();
     },
     addBank() {
-      //this.banks.push({name: '', tca: ''});
+      const newId = this.generateNewId();
+      this.bank.id = newId;
+      this.financeService.create(this.bank).then(response => {
+        let bank = new Bank(response.data);
+        this.banks.push(bank);
+        this.notifySuccessfulAction('Bank created successfully');
+      }).catch(error => {
+        this.$toast.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: `Error creating bank: ${error.message}`,
+          life: 3000
+        });
+      });
 
       this.dialogVisible = false;
       this.clear();
+    },
+    notifySuccessfulAction(message) {
+      if (this.$toast) {
+        this.$toast.add({severity: 'success', summary: 'Success', detail: message, life: 700});
+      } else {
+        console.log('Toast message:', message);
+      }
     }
   }
-
-}
+};
 </script>
 
 <template>
@@ -33,15 +60,15 @@ export default {
   <pv-dialog header="Bank Details" v-model:visible="dialogVisible">
     <pv-float-label>
       <label for="name">Bank Name</label>
-      <pv-input-text id="name" v-model="banks.name" />
+      <pv-input-text id="name" v-model="bank.name"/>
     </pv-float-label>
     <pv-float-label>
-      <label for="tca">Bank TEA</label>
-      <pv-input-number id="tea" v-model="banks.tea" />
+      <label for="tea">Bank TEA</label>
+      <pv-input-number id="tea" v-model="bank.tea"/>
     </pv-float-label>
     <pv-float-label>
       <label for="image">Bank image</label>
-      <pv-input-text id="image" v-model="banks.image" />
+      <pv-input-text id="image" v-model="bank.image"/>
     </pv-float-label>
 
     <pv-button label="Save" @click="addBank"/>
@@ -49,5 +76,4 @@ export default {
 </template>
 
 <style scoped>
-
 </style>
