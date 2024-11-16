f69c074fb26ae2829c9d0c46a29663f5c5577262
P1er0VL <u20211a620@upc.edu.pe>
2024-11-16 03:06:35 -0500

    feat(bills): updated bills service

--- src/bills/services/bills.service.js
+++ src/bills/services/bills.service.js
@@ -4,6 +4,8 @@ export class BillsService {
     resourceEndpoint = '/bills';
 
     getAll() {
+        console.log(http.defaults.baseURL);
+        console.log(this.resourceEndpoint);
         return http.get(this.resourceEndpoint);
     }
 
@@ -11,15 +13,23 @@ export class BillsService {
         return http.get(`${this.resourceEndpoint}/${id}`);
     }
 
+    getByStatus(status) {
+        return http.get(`${this.resourceEndpoint}/${status}`);
+    }
+
     create(bill) {
         return http.post(this.resourceEndpoint, bill);
     }
 
     update(id, bill) {
+        console.log(`Updating bill with ID: ${id}`); // Log the ID
         return http.put(`${this.resourceEndpoint}/${id}`, bill);
     }
 
     delete(id) {
+        console.log(`Deleting bill with ID: ${id}`); // Log the ID
+        console.log(`Request URL: ${this.resourceEndpoint}/${id}`);
+        console.log(this.getById(id))
         return http.delete(`${this.resourceEndpoint}/${id}`);
     }
 
