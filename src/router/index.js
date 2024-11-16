04b856d88fca5d7b55983e7275e6042190754653
P1er0VL <u20211a620@upc.edu.pe>
2024-11-16 03:12:05 -0500

    feat(router): updated indexing with the new pages added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from "vue-router";
 import SignUpComponent from "../IAM/pages/sign-up.component.vue";
 import LoginComponent from "../IAM/pages/login.component.vue";
 import RecoveryComponent from "../IAM/pages/recovery.component.vue";
+import BillsManagementComponent from "../bills/pages/bills-management.component.vue";
+import FinanceManagementComponent from "../finance/pages/finance-management.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +11,8 @@ const router = createRouter({
         { path: '/signup', name: 'signup', component: SignUpComponent, meta: { title: 'Sign Up' } },
         { path: '/login', name: 'login', component: LoginComponent, meta: { title: 'Login' } },
         { path: '/recovery', name: 'recovery', component: RecoveryComponent, meta: { title: 'Recover your password' } },
+        { path: '/bills', name: 'bills', component: BillsManagementComponent, meta: { title: 'Bills Management' } },
+        { path: '/finance', name: 'finance', component: FinanceManagementComponent, meta: { title: 'Finance Management' } },
         { path: '/', redirect: '/login' }
     ]
 })
