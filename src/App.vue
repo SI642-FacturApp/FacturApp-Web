d40cd2f66f0e2d318cdbbfb7244883ed3fcfcb84
pachieeee <sebaspacheco2003@gmail.com>
2024-11-17 02:04:47 -0500

    feat: added temporary account functionality in app.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script>
+import {useRouter} from "vue-router";
+import {useAccountStore} from "./stores/account.store.js";
+
 export default {
   name: 'app',
-  title: 'FacturApp'
+  title: 'FacturApp',
+  data() {
+    return {
+      router: useRouter(),
+      accountStore: useAccountStore()
+    }
+  },
+  computed: {
+    isSignedIn() {
+      return this.accountStore.isSignedIn;
+    }
+  },
+  methods: {
+    onSignOut() {
+      this.accountStore.signOut(this.router);
+    }
+  }
 }
 </script>
 
 <template>
   <main>
+    <div v-if="isSignedIn">
+      <pv-button text @click="onSignOut">Sign Out</pv-button>
+    </div>
     <router-view/>
   </main>
 </template>
