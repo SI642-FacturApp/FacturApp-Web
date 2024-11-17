0b67cfc5bf995863d74b073d09d8a798e0cf303d
pachieeee <sebaspacheco2003@gmail.com>
2024-11-17 15:47:07 -0500

    feat: implemented toolbar.

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,18 @@ export default {
 
 <template>
   <header>
-    <div v-if="isSignedIn" class="fixed top-0">
-      <span class="fixed font-bold p-3 left-0">Bienvenido, {{ currentUsername }}</span>
-      <pv-button text @click="onSignOut" class="m-3 text-white fixed right-0 bg-black-alpha-90 hover:bg-black-alpha-50 border-round-xl border-none">Sign Out</pv-button>
-    </div>
+    <pv-toolbar v-if="isSignedIn" class="fixed top-0 left-0 w-full z-1 h-4rem border-noround">
+      <template #start>
+        <span class="font-bold">Bienvenido, {{ currentUsername }}</span>
+      </template>
+      <template #center>
+        <pv-button text @click="this.router.push('/bills')" class="text-blue-50">Bills</pv-button>
+        <pv-button text @click="this.router.push('/finance')" class="text-blue-50">Banks</pv-button>
+      </template>
+      <template #end>
+        <pv-button text @click="onSignOut" class="text-white bg-black-alpha-90 hover:bg-black-alpha-50 border-round-xl border-none">Sign Out</pv-button>
+      </template>
+    </pv-toolbar>
   </header>
   <main>
     <router-view/>
